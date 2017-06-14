60d2058cd419cf5e5f7b4fd0053336087369b015
Joey Arthur <jgarthur@stanford.edu>
2017-06-14 11:38:12 -0700

    update version and add --version argument

--- setup.py
+++ setup.py
@@ -6,9 +6,14 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     arcsv_license = f.read()
 
+version = '0.0.0'
+for line in open('arcsv/_version.py'):
+    if line.startswith('__version__'):
+        version = line.split('=')[1].strip().strip('"')
+
 setup(
     name='ARC-SV',
-    version='0.9.0',
+    version=version,
     description='Automated Reconstruction of Complex Structural Variants from WGS Data',
     long_description=arcsv_readme,
     author='Joey Arthur',
