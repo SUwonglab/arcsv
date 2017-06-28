1995cf5987ad783b37efbaf8686da348ef7aeee6
Joseph Arthur <joey@Josephs-MacBook-Air-4.local>
2017-06-28 15:33:32 -0700

    update reference annotation script

--- scripts/fetch_reference_annotations.sh
+++ scripts/fetch_reference_annotations.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-###############################################################################
-# This script fetches essential files for ARC-SV from the UCSC database.      #
-# You should NOT need it, but it is provided as an example for how to set up  #
-# the reference genome resources folder.                                      #
-###############################################################################
-
-ARCSV_DIR=/home/jgarthur/git/arcsv # change to your directory
-cd $ARCSV_DIR/arcsv/resources
+##########################################################################
+# This script fetches essential files for ARC-SV from the UCSC database. #
+##########################################################################
 
 # UCSC version
 curl http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/gap.txt.gz | \
